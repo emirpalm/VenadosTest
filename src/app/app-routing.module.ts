57f3047120e8208bf6ca473af04d6029489709da
Emir Nahin <emirpalm@outlook.com>
2019-09-24 17:42:52 -0500

    Se creo el modelo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PlayersComponent } from './pages/players/players.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'Players',
+    component: PlayersComponent,
+    data: { titulo: 'Players' }
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/models/players.models.ts
@@ -0,0 +1,17 @@
+export class Players {
+
+    constructor(
+        public name?: string,
+        public first_surname?: string,
+        public second_surname?: string,
+        public birthday?: Date,
+        public birth_place?: string,
+        public weight?: string,
+        public height?: string,
+        public position?: string,
+        public number?: string,
+        public position_short?: string,
+        public last_team?: string,
+        public image?: string
+    ) {}
+}
